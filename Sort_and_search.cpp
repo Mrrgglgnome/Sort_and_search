1181d6a2c81738303d42ff08abf1b5d3904569d9
Egor <egorbelousko@mail.ru>
2022-02-09 20:15:39 +0700

    быстрая сортировка (починить при одинаковых числах)

--- Sort_and_search.cpp
+++ Sort_and_search.cpp
@@ -21,6 +21,21 @@ vector<char> selection(vector<char> unsorted) {
     return unsorted;
 }
 
+vector<char> quick_sort(vector<char> unsorted) {
+    int size = unsorted.size();
+    if (size > 1) {
+        vector<char> half_1, half_2;
+        char comparand = unsorted[size / 2];
+        for (int i = 0; i < size; i++)
+            unsorted[i] <= comparand ? half_1.push_back(unsorted[i]) : half_2.push_back(unsorted[i]);
+        vector<char> sorted_1 = quick_sort(half_1), sorted_2 = quick_sort(half_2);
+        sorted_1.insert(sorted_1.end(), sorted_2.begin(), sorted_2.end());
+        return sorted_1;
+    }
+    else
+        return unsorted;
+}
+
 int main()
 {
     vector<char> input;
@@ -30,7 +45,7 @@ int main()
         input.push_back(in);
     }
 
-    vector<char> sorted = selection(input);
+    vector<char> sorted = quick_sort(input);
 
     for (int i = 0; i < 5; i++) {
         cout << sorted[i] << " ";
