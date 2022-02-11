61e16f34a7905ee96e0c3f1609401de5e512b2da
Egor <egorbelousko@mail.ru>
2022-02-11 14:25:35 +0700

    Закончил быструю сортировку + добавил комментарии

--- Sort_and_search.cpp
+++ Sort_and_search.cpp
@@ -1,21 +1,22 @@
 ﻿
 #include <iostream>
 #include <vector>
+#include <numeric>
 
 using namespace std;
 
-vector<char> selection(vector<char> unsorted) {
-    int last_index = unsorted.size() - 1;
+vector<char> selection_sort(vector<char> unsorted) {
+    int last_index = unsorted.size() - 1;           // Индекс последнего элемента для записи в конец найденого максимального элемента
     while (last_index != 0) {
         char max = CHAR_MIN;
-        int index_max = 0;
+        int index_max = 0;                          // Индекс максимального элемента
         for (int i = 0; i <= last_index; i++) {
             if (max < unsorted[i]) {
                 max = unsorted[i];
                 index_max = i;
             }
         }
-        unsorted[index_max] = unsorted[last_index];
+        unsorted[index_max] = unsorted[last_index]; // Обмен местами максимального элемента с последним в неотсортированной части массива
         unsorted[last_index--] = max;
     }
     return unsorted;
@@ -23,17 +24,17 @@ vector<char> selection(vector<char> unsorted) {
 
 vector<char> quick_sort(vector<char> unsorted) {
     int size = unsorted.size();
-    if (size > 1) {
+    while (true) {
         vector<char> half_1, half_2;
-        char comparand = unsorted[size / 2];
+        char comparand = accumulate(unsorted.begin(), unsorted.end(), 0) / size;                        // Вычисление компаранда как среднее арифмет. массива
         for (int i = 0; i < size; i++)
-            unsorted[i] <= comparand ? half_1.push_back(unsorted[i]) : half_2.push_back(unsorted[i]);
-        vector<char> sorted_1 = quick_sort(half_1), sorted_2 = quick_sort(half_2);
-        sorted_1.insert(sorted_1.end(), sorted_2.begin(), sorted_2.end());
+            unsorted[i] <= comparand ? half_1.push_back(unsorted[i]) : half_2.push_back(unsorted[i]);   // Разделение массива на два с теми, что меньше и теми, что больше компаранда
+        if (half_2.empty())     // Проверка для выхода из рекурсии
+            return unsorted;
+        vector<char> sorted_1 = quick_sort(half_1), sorted_2 = quick_sort(half_2);                      // Рекурсивное повторение
+        sorted_1.insert(sorted_1.end(), sorted_2.begin(), sorted_2.end());                              // Слияние массивов в отсортированный
         return sorted_1;
     }
-    else
-        return unsorted;
 }
 
 int main()
